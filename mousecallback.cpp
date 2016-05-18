967497ffb52d94abfba743594912828cab3f631e
gsengupta2810 <gsengupta2810@gmail.com>
2016-05-18 18:02:28 +0530

    mouse callback added

---
+++ mousecallback.cpp
@@ -0,0 +1,58 @@
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int check=5;
+
+void CallBackFunc(int event, int x, int y, int flags, void* userdata)
+{
+     if  ( event == EVENT_LBUTTONDOWN )
+     {
+          cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+     }
+     else if  ( event == EVENT_RBUTTONDOWN )
+     {
+          cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+     }
+     else if  ( event == EVENT_MBUTTONDOWN )
+     {
+          cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+     }
+     else if ( event == EVENT_MOUSEMOVE )
+     {
+          cout << "Mouse move over the window - position (" << x << ", " << y << ")" << endl;
+
+     }
+     check=10;
+}
+
+int main(int argc, char** argv)
+{
+     // Read image from file 
+     Mat img = imread("lena.jpg");
+
+      //if fail to read the image
+     if ( img.empty() ) 
+     { 
+          cout << "Error loading the image" << endl;
+          return -1; 
+     }
+
+      //Create a window
+     namedWindow("My Window", 1);
+
+      //set the callback function for any mouse event
+     setMouseCallback("My Window", CallBackFunc, NULL);
+
+      //show the image
+     imshow("My Window", img);
+
+     // Wait until user press some key
+     waitKey(0);
+     if(check==10)
+     cout<<"checking out"<<check<<endl;
+      return 0;
+
+}
